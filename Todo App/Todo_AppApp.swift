89094d14615cf761d21b951a64bce1ded5a56d3f
burkinse <burakurtuldu.se@gmail.com>
2025-01-31 21:38:43 +0100

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Burak 2 on 2025-01-31.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todo AppTests/Todo_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppTests.swift
+//  Todo AppTests
+//
+//  Created by Burak 2 on 2025-01-31.
+//
+
+import Testing
+@testable import Todo_App
+
+struct Todo_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
